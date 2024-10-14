197ab16dce61c28a27a8bc120b24b30b2c1c8c5f
Preetham <96488473+Sa1Preetham@users.noreply.github.com>
2024-10-14 16:42:42 -0700

    [1423] [Time Beats: 99.06%] [Memory Beats: 40.57%] - LeetPush

--- 1423-Maximum-Points-You-Can-Obtain-from-Cards.py
+++ 1423-Maximum-Points-You-Can-Obtain-from-Cards.py
@@ -2,14 +2,16 @@ class Solution:
     def maxScore(self, arr: List[int], k: int) -> int:
         if k == len(arr):
             return sum(arr)
-        sums = [sum(arr[:k]), ]
+        m = sum(arr[:k])
         left = k-1
-        s = sums[0]
+        s = m
         right = len(arr)-1
         while left >= 0:
             s = s - arr[left] + arr[right]
+            if s > m:
+                m = s
             left-=1
             right-=1
-            sums.append(s)
+            # sums.append(s)
             
-        return max(sums)
+        return m
