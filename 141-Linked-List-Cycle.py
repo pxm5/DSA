59dd41a5a6ac22241d9fb3eaefaa6fb25c677005
Preetham <96488473+Sa1Preetham@users.noreply.github.com>
2024-11-02 15:20:54 -0700

    [141] [Time Beats: 44.40%] [Memory Beats: 8.76%] - LeetPush

---
+++ 141-Linked-List-Cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        objs = {}
+        itr = head
+        
+        while itr is not None:
+            if itr not in objs:
+                objs[itr] = 1
+                itr = itr.next
+            elif itr in objs:
+                return True
+        return False
